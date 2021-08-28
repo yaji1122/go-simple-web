fea9fbc798f9c67460c849a05779e7b7126e7e4a
yaji <yajilin@fontrip.com>
2021-08-28 21:56:37 +0800

    import chi for route, use middleware Recoverer

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/yaji1122/go-simple-web/pkg/config"
 	"github.com/yaji1122/go-simple-web/pkg/handler"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handler.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handler.Repo.About))
-	return mux
+	//mux := pat.New()
+	//mux.Get("/", http.HandlerFunc(handler.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handler.Repo.About))
+	router := chi.NewRouter()
+	router.Use(middleware.Recoverer)
+	router.Get("/", handler.Repo.Home)
+	router.Get("/about", handler.Repo.About)
+	return router
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/yaji1122/go-simple-web
 
 go 1.16
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.0.3
